f86811fd6d1391a32661515105f92b7b8f6b95fe
henataso <pxqth56v@icloud.com>
2025-02-08 15:01:02 +0900

    プルリクエスト修正５

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'only allows valid emails' }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
