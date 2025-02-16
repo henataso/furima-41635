8f5fffbcb0eab9ba944becff48c720a697308f0e
henataso <pxqth56v@icloud.com>
2025-02-16 15:39:36 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,28 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.order("created_at DESC
-    ")
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
     @user = current_user
+    @categories = Category.all.map { |c| [c.name, c.id] }
+    @conditions = Condition.all.map { |c| [c.name, c.id] }
+    @shipping_costs = ShippingCost.all.map { |c| [c.name, c.id] }
+    @regions = Region.all.map { |c| [c.name, c.id] }
+    @delivery_times = DeliveryTime.all.map { |c| [c.name, c.id] }
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to @item
+      redirect_to root_path, notice: 'Item was successfully created.'
     else
-      @categories = Category.all.pluck(:itemname, :category_id)
-      @statuses = Item.statuses.keys.map { |status| [status.titleize, status] }
+      @categories = Category.all.map { |c| [c.name, c.id] }
+      @conditions = Condition.all.map { |c| [c.name, c.id] }
+      @shipping_costs = ShippingCost.all.map { |c| [c.name, c.id] }
+      @regions = Region.all.map { |c| [c.name, c.id] }
+      @delivery_times = DeliveryTime.all.map { |c| [c.name, c.id] }
+      flash.now[:alert] = 'There was a problem with your submission.'
       render :new
     end
   end
@@ -23,6 +30,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:itemname, :category_id)
+    params.require(:item).permit(:itemname, :itemtext, :category_id, :condition_id, :shipping_cost_id, :region_of_origin_id,
+                                 :delivery_time_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,23 +1,27 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category_id
-  belongs_to :condition_id
-  belongs_to :shipping_cost_id
-  belongs_to :region_of_origin_id
-  belongs_to :delivery_time_id
 
-  validates :itemname, presence: true
-  validates :itemtext, presence: true
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost, class_name: 'ShippingCost'
+  belongs_to_active_hash :region, class_name: 'Region'
+  belongs_to_active_hash :delivery_time, class_name: 'DeliveryTime'
+
+  belongs_to :user
+
+  has_one_attached :image
+
+  validates :itemname, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :itemtext, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_cost_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :region_of_origin_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_time_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true
-  validates :image, presence: true, unless: :was_attached?
+  validates :price, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image, presence: true, unless: :was_attached?, numericality: { other_than: 1, message: "can't be blank" }
 
   def was_attached?
     image.attached?
   end
 end
-shipping_cost_id
