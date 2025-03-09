50343e8e17eba65f27f65acfe34fb7923ac4e4a0
henataso <pxqth56v@icloud.com>
2025-03-09 15:56:13 +0900

    単体テストコード修正

--- spec/factories/order_address_forms.rb
+++ spec/factories/order_address_forms.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :order_address_form do
-    
+    price { 3000 }
+    token { 'tok_abcdefghijk00000000000000000' }
+    user_id { FactoryBot.create(:user).id }
+    item_id { FactoryBot.create(:item).id }
+    postal_code { '123-4567' }
+    region_of_origin_id { 2 }
+    city { '東京都' }
+    house_number { '1-1' }
+    building_name { 'ビル名' }
+    tell { '09012345678' }
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Order, type: :model do
+RSpec.describe OrderAddressForm, type: :model do
   before do
-    @order = FactoryBot.build(:order)
+    @order_address_form = FactoryBot.build(:order_address_form)
   end
 
-  context '内容に問題ない場合' do
+  describe '内容に問題ない場合' do
     it 'priceとtokenがあれば保存ができること' do
-      expect(@order).to be_valid
+      expect(@order_address_form).to be_valid
     end
   end
 
-  context '内容に問題がある場合' do
+  describe '内容に問題がある場合' do
     it 'priceが空では保存ができないこと' do
-      @order.price = nil
-      @order.valid?
-      expect(@order.errors.full_messages).to include("Price can't be blank")
+      @order_address_form.price = nil
+      @order_address_form.valid?
+      expect(@order_address_form.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'tokenが空では登録できないこと' do
-      @order.token = nil
-      @order.valid?
-      expect(@order.errors.full_messages).to include("Token can't be blank")
+      @order_address_form.token = nil
+      @order_address_form.valid?
+      expect(@order_address_form.errors.full_messages).to include("Token can't be blank")
     end
   end
 end
